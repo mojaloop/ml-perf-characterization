c2f0195e0b51bc58238b6eab3b644ad03c544e2e
Vijay <vijaya.guthi@infitx.com>
2023-09-21 17:16:12 +0530

    feat: added large file storage

--- cpu-profiling/cl-position-prepare-batch-handler/test-data/feed-test-data-position-prepare-batch.sh
+++ cpu-profiling/cl-position-prepare-batch-handler/test-data/feed-test-data-position-prepare-batch.sh
@@ -1,12 +1,16 @@
 TOPIC_NAME=topic-transfer-position-batch
 # TOPIC_NAME=topic-transfer-position
 
-docker exec -t kafka kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic $TOPIC_NAME
-docker exec -t kafka kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic $TOPIC_NAME --replication-factor 1 --partitions 19
+# docker exec -t kafka kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic $TOPIC_NAME
+# docker exec -t kafka kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic $TOPIC_NAME --replication-factor 1 --partitions 19
 
-tar -xvzf cl-position-handler-testing-prepare-50k.tar.gz
+# position-prepare-5k-2dfsps.tar.gz
+# position-prepare-5l-2dfsps.tar.gz
+# position-prepare-5l-4dfsps.tar.gz
+# position-prepare-5l-8dfsps.tar.gz
+tar -xvzf position-prepare-5k-2dfsps.tar.gz
 
-cat cl-position-handler-testing-prepare-50k.sql | docker exec -i mysql-cl /bin/mysql central_ledger
-cat kafka-topic-transfer-position-prepare-50k.dump | kcat -b localhost:9092 -t $TOPIC_NAME -X topic.partitioner=murmur2_random -K\|
+cat cl-position-handler-testing-prepare.sql | docker exec -i mysql-cl /bin/mysql central_ledger
+cat kafka-topic-transfer-position-prepare.dump | docker run -i --log-driver=none -a stdin -a stdout -a stderr --network=host edenhill/kcat:1.7.1 -b localhost:9092 -t $TOPIC_NAME -X topic.partitioner=murmur2_random -K\|
 
-rm cl-position-handler-testing-prepare-50k.sql kafka-topic-transfer-position-prepare-50k.dump
+rm cl-position-handler-testing-prepare.sql kafka-topic-transfer-position-prepare.dump
